5f9fb2f1128a72274fd431c289748944f236f8aa
L00188525 <l00188525@atu.ie>
2024-11-29 21:29:38 +0000

    Feat: Add unit test project
    Adding unit test project object and solution files.

---
+++ UnitTestLabTests/Test1.cs
@@ -0,0 +1,29 @@
+﻿using UnitTestingLab;
+
+namespace UnitTestLabTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        // Adding test to verify that the default number of doors is 1
+        [TestMethod]
+        public void TestGetDoorNumber_Default()
+        {
+            int result = Car.getDoorNumber();
+            Assert.AreEqual(1, result, "The default number of doors should be 1.");
+        }
+
+        // Adding est to verify setting a valid number of doors 1 to 5
+        [TestMethod]
+        public void TestSetDoorNumber_Valid()
+        {
+            int validNumber = 3;
+
+            Car.setDoorNumber(validNumber);
+            int result = Car.getDoorNumber();
+
+            Assert.AreEqual(validNumber, result, "The number of doors should be set to the valid input.");
+        }
+
+    }
+}
